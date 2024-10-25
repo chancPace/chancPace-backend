2993a92e92605b67c75732671d6d29de531e6c80
FED <105217969+scn2930@users.noreply.github.com>
2024-10-25 19:00:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import userRouter from './router/user.js';
 import paymentRouter from './router/payment.js';
 import spaceRouter from './router/space.js';
-import categoryRouter from './router/category.js'
+import categoryRouter from './router/category.js';
 
 dotenv.config();
 
@@ -24,12 +24,12 @@ app.get('/', (req, res) => {
 app.use('/api/user', userRouter);
 app.use('/api/payment', paymentRouter);
 app.use('/api/space', spaceRouter);
-app.use('/api/category', categoryRouter)
+app.use('/api/category', categoryRouter);
 
 db.sequelize
   // alter: true / 데이터 유지하고 구조만 업데이트
   // force: true / 데이터 초기화 하고 구조 업데이트
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     console.log('데이터베이스와 모델 동기화 완료');
     app.listen(PORT, () => {
