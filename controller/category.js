5f74d4c35de436c8e4381f2d60a6f4f6835dfa97
Y0_0N98 <shift71895@gmail.com>
2024-10-28 19:50:03 +0900

    Fix: 카테고리 삭제 기능 추가
    24.10.28

--- controller/category.js
+++ controller/category.js
@@ -82,130 +82,39 @@ export const getCategory = async (req, res) => {
 };
 
 //ANCHOR - 카테고리 삭제
-
-//NOTE - --------
-
-// ANCHOR - 카테고리 대분류 조회
-export const getBigCategory = async (req, res) => {
+export const removeCategory = async (req, res) => {
   try {
-    const categories = await Category.findAll({ where: { pId: null } });
+    const { id } = req.body;
+    // 삭제할 카테고리 조회
+    const getCategory = await Category.findOne({ where: { id } });
 
-    res.status(200).json({
-      result: true,
-      data: categories,
-      message: '카테고리 조회 성공',
-    });
-  } catch (error) {
-    console.log('카테고리 에러 발생: ', error);
-
-    res.status(500).json({
-      result: false,
-      message: '카테고리 조회 중 오류가 발생했습니다.',
-      error,
-    });
-  }
-};
-
-//ANCHOR - 카테고리 소분류 조회
-export const getSmallCategory = async (req, res) => {
-  try {
-    const categories = await Category.findAll({
-      where: {
-        pId: { [Op.ne]: null }, // null이 아닌 소분류만 조회 / Op 연산자 사용
-      },
-    });
-    res.status(200).json({
-      result: true,
-      data: categories,
-      message: '카테고리 조회 성공',
-    });
-  } catch (error) {
-    res.status(500).json({
-      result: false,
-      message: '카테고리 조회 중 오류가 발생했습니다.',
-      error,
-    });
-  }
-};
-
-//ANCHOR - 대분류에 해당하는 소분류 조회
-export const getSmallCategoriesByBigCategory = async (req, res) => {
-  try {
-    const { bigCategoryId } = req.body;
-
-    const bigCategory = await Category.findOne({ where: { id: bigCategoryId } });
-    if (!bigCategory) {
+    // 카테고리가 존재하지 않는 경우
+    if (!getCategory) {
       return res.status(404).json({
         result: false,
-        message: '대분류가 존재하지 않습니다.',
+        message: '삭제 할 카테고리가 존재하지 않습니다.',
       });
     }
 
-    const categories = await Category.findAll({ where: { pId: bigCategoryId } });
-    res.status(200).json({
-      result: true,
-      data: categories,
-      message: '카테고리 조회 성공',
-    });
-  } catch (error) {
-    res.status(500).json({
-      result: false,
-      message: '카테고리 조회 중 오류가 발생했습니다.',
-      error,
-    });
-  }
-};
-
-//ANCHOR - 카테고리 대분류 삭제
-export const removeBigCategory = async (req, res) => {
-  try {
-    const { categoryName } = req.body;
-
-    // 대분류 조회
-    const bigCategory = await Category.findOne({ where: { categoryName, pId: null } });
-
-    if (!bigCategory) {
-      return res.status(404).json({
-        result: false,
-        message: '삭제할 대분류 카테고리를 찾을 수 없습니다.',
-      });
-    }
-
-    // 대분류에 속한 소분류들 삭제
-    await Category.destroy({ where: { pId: bigCategory.id } });
-    // 대분류 삭제
-    await bigCategory.destroy();
-
-    res.status(200).json({
-      result: true,
-      message: `${categoryName} 대분류 및 해당 소분류들을 삭제했습니다.`,
-    });
-  } catch (error) {
-    res.status(500).json({
-      result: false,
-      message: '카테고리 삭제 중 오류가 발생했습니다.',
-      error,
-    });
-  }
-};
-
-//ANCHOR - 카테고리 소분류 삭제
-export const removeSmallCategory = async (req, res) => {
-  try {
-    const { categoryName, bigCategoryId } = req.body;
-
-    const deletedCount = await Category.destroy({ where: { categoryName, pId: bigCategoryId } });
-
-    if (deletedCount > 0) {
+    // 대분류인 경우
+    if (getCategory.pId === null) {
+      // 하위 소분류 삭제
+      await Category.destroy({ where: { pId: getCategory.id } });
+      // 대분류 삭제
+      await Category.destroy({ where: { id: getCategory.id } });
       res.status(200).json({
         result: true,
-        message: `${categoryName} 소분류 카테고리를 삭제했습니다.`,
+        message: '대분류 및 하위 소분류를 삭제 완료했습니다.',
       });
+
     } else {
-      res.status(404).json({
-        result: false,
-        message: '삭제할 소분류 카테고리를 찾을 수 없습니다.',
+      // 소분류인 경우
+      await Category.destroy({ where: { id: getCategory.id } });
+      res.status(200).json({
+        result: true,
+        message: '소분류 카테고리를 삭제 완료했습니다.',
       });
+
     }
   } catch (error) {
     res.status(500).json({
--- router/category.js
+++ router/category.js
@@ -1,13 +1,5 @@
 import express from 'express';
-import {
-  addCategory,
-  getBigCategory,
-  getCategory,
-  getSmallCategoriesByBigCategory,
-  getSmallCategory,
-  removeBigCategory,
-  removeSmallCategory,
-} from '../controller/category.js';
+import { addCategory, getCategory, removeCategory } from '../controller/category.js';
 
 const router = express.Router();
 
@@ -17,12 +9,7 @@ router.post('/add-category', addCategory);
 //ANCHOR - 카테고리 조회
 router.get('/get-category', getCategory);
 
-router.get('/get-big-category', getBigCategory);
-router.get('/get-small-category', getSmallCategory);
-router.get('/get-small-category-by-big-category', getSmallCategoriesByBigCategory);
-
 //ANCHOR - 카테고리 삭제
-router.delete('/remove-big-category', removeBigCategory);
-router.delete('/remove-small-category', removeSmallCategory);
+router.delete('/remove-category', removeCategory);
 
 export default router;
