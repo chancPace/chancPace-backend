ba60974606475022533b2d9d98a4652953243ea3
Y0_0N98 <shift71895@gmail.com>
2024-11-06 17:21:30 +0900

    Fix: 찜등록 라우터 연결
    24.11.06

--- router/wishlist.js
+++ router/wishlist.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {} from '../controller/wishlist.js';
+import { addWishlist } from '../controller/wishlist.js';
 
 const router = express.Router();
 
 //ANCHOR - 찜 등록
+router.post('/add-wishlist', addWishlist);
 
 export default router;
