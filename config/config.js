7dfb7a7f463f595ce1d2d7425a5b9006969686ed
Y0_0N98 <shift71895@gmail.com>
2024-11-16 17:56:48 +0900

    Fix: db 연결 설정 변경
    24.11.16

--- config/config.js
+++ config/config.js
@@ -1,25 +1,18 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 const config = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_LOCAL,
+    password: process.env.DB_PASSWORD_LOCAL,
+    database: process.env.DB_DATABASE_LOCAL,
+    host: process.env.DB_HOST_LOCAL,
+    port: process.env.LOCAL_PORT, // 로컬 포트 추가 확인
     dialect: 'mysql',
-    serverHost: process.env.LOCAL_HOST,
-    serverPort: process.env.LOCAL_PORT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_AWS,
+    password: process.env.DB_PASSWORD_AWS,
+    database: process.env.DB_DATABASE_AWS,
+    host: process.env.DB_HOST_AWS,
+    port: process.env.DB_PORT_AWS, // AWS 포트 추가
     dialect: 'mysql',
-    serverHost: process.env.SERVER_HOST,
-    serverPort: process.env.SERVER_PORT,
   },
 };
-
-export default config;
